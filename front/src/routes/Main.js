0b6b86699701aa78a006761716ca75e0fdbd6442
Kim JH Desktop <kim_jh6859@naver.com>
2023-04-03 23:53:05 +0900

    헤더 대략적으로

--- front/src/routes/Main.js
+++ front/src/routes/Main.js
@@ -1,11 +1,12 @@
+import Header from '../components/Header';
+
 import styles from "./css/Main.module.css";
 
 function Main() {
   return (
     <div className={styles.mainPageContainer}>
       <div className={styles.headerContainer}>
-        <div className={styles.logoHeaderContainer}>logo</div>
-        <div className={styles.menuHedaerContainer}>menu</div>
+        <Header />
       </div>
       <div className={styles.mainImageContainer}>mainImage</div>
       <div className={styles.contentContainer}>content</div>
