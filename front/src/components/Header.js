93cf138bfacc5b084807d534d2016200fffb0014
Kim JH Desktop <kim_jh6859@naver.com>
2023-04-04 03:42:59 +0900

    Main 틀

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -3,18 +3,18 @@ import styles from './css/Header.module.css';
 
 function Header() {
     return (
-        <div>
+        <div className={styles.container}>
             <div className={styles.logoContainer}>
                 <Link to='/' ><img src='./logo.png' /></Link>
             </div>
             <div className={styles.menuContainer}>
                 <div className={styles.menus}>
-                    <Link to='/boardviewrecommand'>여행지 추천</Link>
+                    <Link to='/boardviewrecommand' className={styles.link}>여행지 추천</Link>
                 </div>
                 <div className={styles.menus}>
-                    <Link to='/regions'>관광지 보기</Link>
+                    <Link to='/regions' className={styles.link}>관광지 보기</Link>
                 </div><div className={styles.menus}>
-                    <Link to='#'>게시판</Link>
+                    <Link to='#' className={styles.link}>게시판</Link>
                 </div>
             </div>
         </div>
--- front/src/routes/Main.js
+++ front/src/routes/Main.js
@@ -1,14 +1,30 @@
+import { useState } from 'react';
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
+
 import Header from '../components/Header';
 
 import styles from "./css/Main.module.css";
 
 function Main() {
+  const [search, setSearch] = useState("");
+  const onSearchChange = (e) => {
+    setSearch(e.target.value)
+  }
   return (
     <div className={styles.mainPageContainer}>
-      <div className={styles.headerContainer}>
-        <Header />
+      <Header />
+      <div className={styles.imageContainer}>
+        <AiFillCaretLeft size={50} color='grey' className={styles.imageLeft} />
+        <AiFillCaretRight size={50} color='grey' className={styles.imageRight} />
+        <input
+          type="text"
+          value={search}
+          onChange={onSearchChange}
+          placeholder="검색어를 입력해주세요."
+          className={styles.search}
+        />
+
       </div>
-      <div className={styles.mainImageContainer}>mainImage</div>
       <div className={styles.contentContainer}>content</div>
     </div>
   );
