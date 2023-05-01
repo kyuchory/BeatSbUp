15bb1e27ffcb910ec8e7315a461ea9a6427a48bb
yong-won-kim <qsc753949@gmail.com>
2023-05-01 22:30:53 +0930

    db link

--- back/server.js
+++ back/server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "루트 말고 딴걸로 해야할듯",
+  password: "본인 비밀번호",
+  database: "travel"
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
@@ -10,10 +18,16 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('HelloWorld')
 })
+//DB테스트
 app.get('/test', (req, res) => {
-    console.log('hi')
-    res.json({message: 'This is a test endpoint'})
+    connection.query('select * from sight',
+    function(error,results,fields){
+      console.log(results)
+      if(error) throw error;
+      res.json(results);
+    })
 })
+
 app.listen(3001, () => {
   console.log('3001 port running')
 })
