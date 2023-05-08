c4d28695f8e8fc2389a3c4e7a04ba4ceaabc6486
Kim JH Desktop <kim_jh6859@naver.com>
2023-05-09 01:29:39 +0900

    검색 데이터 기본 구현

--- front/src/routes/SearchDetail.js
+++ front/src/routes/SearchDetail.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import Header from '../components/Header';
+import styles from './css/SearchDetail.module.css'
 
 function SearchDetail() {
     const location = useLocation();
     const searchText = location.state.searchText;
 
-    const [res, setRes] = useState();
+    const [res, setRes] = useState(null);
 
-    useEffect(async () => {
-        await axios.get(`https://apis.data.go.kr/B551011/KorService1/searchKeyword1?serviceKey=SaXEWBrqfLH2I6uYF88gUq7wTPmI7VxP7lAvYCJmsAo80LmwmPB8tDMoZRM3%2Bo39PLk32tOm6exWqvROqh0aDg%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&keyword=${searchText}&contentTypeId=12`)
+    useEffect(() => {
+        axios
+            .get(
+                `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?serviceKey=SaXEWBrqfLH2I6uYF88gUq7wTPmI7VxP7lAvYCJmsAo80LmwmPB8tDMoZRM3%2Bo39PLk32tOm6exWqvROqh0aDg%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&keyword=${searchText}&contentTypeId=12`
+            )
             .then(response => {
                 setRes(response.data.response.body.items.item);
                 console.log(response.data.response.body.items.item);
@@ -21,8 +26,23 @@ function SearchDetail() {
 
     return (
         <div>
-            안녕하세요
-            {JSON.stringify(res)}
+            <Header />
+            <div className={styles.content}>
+                {res ? (
+                    <ul className={styles.lists}>
+                        <li className={styles.list}>
+                            <div className={styles.imageContainer}>
+                                <img src="./logo.png" />
+                            </div>
+                        </li>
+                        {res.map(item => (
+                            <li key={item.contentid}>{item.title}</li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
         </div>
     );
 }
