5d06cfeb1af1e360de56ac83d3db8e73773164be
Kim JH Desktop <kim_jh6859@naver.com>
2023-05-12 21:39:50 +0900

    간단 정리

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,13 +1,30 @@
+import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import styles from './css/Header.module.css';
 import { CgProfile } from "react-icons/cg";
 
+import axios from 'axios';
+
 function Header() {
+    const [isLogin, setIsLogin] = useState(false);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/authCheck")
+            .then((response) => {
+                setIsLogin(response.data.isLogin);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <div className={styles.container}>
             <div className={styles.logoContainer}>
                 <Link to='/' ><img src='./logo.png' /></Link>
             </div>
+            {/* {isLogin ? '로그아웃' : '로그인'} */}
+            {/* 추후에 로그인, 로그아웃 다르게 설정 */}
             <div className={styles.profile}>
                 <CgProfile size={'50px'} color='rgb(72, 72, 72)' cursor='pointer' />
                 <div className={styles.profileSelector}>
