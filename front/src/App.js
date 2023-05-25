c67bbbf9611a5e5c2d0eab111d2e434042bbb0c0
Kim JH Desktop <kim_jh6859@naver.com>
2023-05-26 01:32:11 +0900

    추천 페이지

--- front/src/App.js
+++ front/src/App.js
@@ -26,6 +26,7 @@ import BoardView_party from "./routes/board_party/BoardView_party.js";
 import BoardWrite_party from "./routes/board_party/BoardWrite_party.js";
 
 import Recommand from "./routes/recommand/Recommand.js";
+import RecommandDetail from "./routes/recommand/RecommandDetail.js";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -51,17 +52,21 @@ function App() {
         <Route path="/addschedule" element={<AddSchedule />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/searchDetail" element={<SearchDetail />} />
+
         <Route path="/regionmain" element={<RegionMain />} />
         <Route path="/regions" element={<Regions />} />
         <Route path="/regiondetail" element={<RegionDetail />} />
         <Route path="/regionattraction" element={<RegionAttraction />} />
+
         <Route path="/BoardList" element={<BoardList />} />
         <Route path="/BoardView" element={<BoardView />} />
         <Route path="/BoardWrite" element={<BoardWrite />} />
         <Route path="/BoardList_party" element={<BoardList_party />} />
         <Route path="/BoardView_party" element={<BoardView_party />} />
         <Route path="/BoardWrite_party" element={<BoardWrite_party />} />
+
         <Route path="/recommand" element={<Recommand />} />
+        <Route path="/recommanddetail" element={<RecommandDetail />} />
       </Routes>
     </Router>
   );
--- front/src/routes/region/RegionMain.js
+++ front/src/routes/region/RegionMain.js
@@ -5,45 +5,45 @@ function RegionMain() {
     return (
         <div className={styles.container}>
             <div className={styles.main}>
-            <div className={styles.side}>
-                <div className={styles.side_image}>
-                    <div className={styles.regionImage}></div>
-                    <p className={styles.regionName}>서울</p>
+                <div className={styles.side}>
+                    <div className={styles.side_image}>
+                        <div className={styles.regionImage}></div>
+                        <p className={styles.regionName}>서울</p>
+                    </div>
+                    <div className={styles.side_image}>
+                        <div className={styles.regionImage}></div>
+                        <p className={styles.regionName}>서울</p>
+                    </div>
+                    <div className={styles.side_image}>
+                        <div className={styles.regionImage}></div>
+                        <p className={styles.regionName}>서울</p>
+                    </div>
+                    <div className={styles.side_image}>
+                        <div className={styles.regionImage}></div>
+                        <p className={styles.regionName}>서울</p>
+                    </div>
                 </div>
-                <div className={styles.side_image}>
-                    <div className={styles.regionImage}></div>
-                    <p className={styles.regionName}>서울</p>
-                </div>
-                <div className={styles.side_image}>
-                    <div className={styles.regionImage}></div>
-                    <p className={styles.regionName}>서울</p>
-                </div>
-                <div className={styles.side_image}>
-                    <div className={styles.regionImage}></div>
-                    <p className={styles.regionName}>서울</p>
+                <div className={styles.center}></div>
+                <div className={styles.side}>
+                    <div className={styles.side_image}>
+                        <div className={styles.regionImage}></div>
+                        <p className={styles.regionName}>서울</p>
+                    </div>
+                    <div className={styles.side_image}>
+                        <div className={styles.regionImage}></div>
+                        <p className={styles.regionName}>서울</p>
+                    </div>
+                    <div className={styles.side_image}>
+                        <div className={styles.regionImage}></div>
+                        <p className={styles.regionName}>서울</p>
+                    </div>
+                    <div className={styles.side_image}>
+                        <div className={styles.regionImage}></div>
+                        <p className={styles.regionName}>서울</p>
+                    </div>
                 </div>
             </div>
-            <div className={styles.center}></div>
-            <div className={styles.side}>
-                <div className={styles.side_image}>
-                    <div className={styles.regionImage}></div>
-                    <p className={styles.regionName}>서울</p>
-                </div>
-                <div className={styles.side_image}>
-                    <div className={styles.regionImage}></div>
-                    <p className={styles.regionName}>서울</p>
-                </div>
-                <div className={styles.side_image}>
-                    <div className={styles.regionImage}></div>
-                    <p className={styles.regionName}>서울</p>
-                </div>
-                <div className={styles.side_image}>
-                    <div className={styles.regionImage}></div>
-                    <p className={styles.regionName}>서울</p>
-                </div>
-            </div>
-            </div>
-            
+
         </div>
     );
 }
