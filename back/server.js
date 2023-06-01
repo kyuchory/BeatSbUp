f7e8ee5a0df8a3ba0605c04a09185735b561704c
yong-won-kim <qsc753949@gmail.com>
2023-06-01 13:09:48 +0930

    fetch schedule

--- back/server.js
+++ back/server.js
@@ -37,11 +37,11 @@ var sessionOption = {
   password: "test1234",
   database: "travel",
   port: 3306,
-
   clearExpired: true, // 만료된 세션 자동 확인 및 지우기 여부
   checkExpirationInterval: 10000, // 만료된 세션이 지워지는 빈도 (milliseconds)
   expiration: 1000 * 60 * 60 * 2, // 유효한 세션의 최대 기간 2시간으로 설정 (milliseconds)
 };
+
 app.use(
   session({
     key: "session_cookie_name",
--- front/src/routes/schedule/Floating.js
+++ front/src/routes/schedule/Floating.js
@@ -1,32 +1,121 @@
+import { useEffect, useState } from "react";
 import styles from "./Floating.module.css";
+import axios from "axios";
+import * as dayjs from "dayjs";
 function Floating() {
+  const [data, setData] = useState([])
+  const [selected, setSelected] = useState(0)
+  const [selected2, setSelected2] = useState(0)
+  const [date, setDate] = useState([])
+  const [sch,setSch] = useState([])
+
+  function select(index) {
+    setSelected(index);
+  }
+
+  function select2(index) {
+    setSelected2(index);
+  }
+
+  function fetchData() {
+    axios
+      .get("http://localhost:3001/gathering/select", {
+        params: {
+          user: 12345,
+        },
+      })
+      .then(function (response) {
+        setData(response.data);
+      });
+  }
+  function fetchGathering() {
+    if (data[selected]?.name && data[selected]?.admin) {
+      axios
+        .get("http://localhost:3001/schedule/checkDate", {
+          params: {
+            name: data[selected].name,
+            admin: data[selected].admin,
+          },
+        })
+        .then(function (response) {
+          setDate(response.data);
+        });
+    }
+  }
+  function fetchSch(){
+    if (date[0]?.id) {
+    axios
+        .get("http://localhost:3001/schedule/getSchedule", {
+          params: {
+            id : date[0]?.id,
+            offset : selected2
+        },
+        })
+        .then(function (response) {
+          setSch(response.data);
+          console.log(sch)
+        });
+  }}
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    fetchGathering();
+  }, [data, selected]);
+
+  useEffect(() => {
+    fetchSch();
+  }, [selected2]);
+
   return (
     <div className={styles.container}>
       <div className={styles.selGath}>
-        <button className={styles.selButton}>모임1</button>
-        <button className={styles.selButton}>모임2</button>
-        <button className={styles.selButton}>모임3</button>
-        <button className={styles.selButton}>모임4</button>
+        {data.length === 0 ? (
+          <p>참가한 모임이 없습니다.</p>
+        ) : (
+          data.map((item, index) => (
+            index === selected ? (
+              <button onClick={() => select(index)} className={styles.selButtons}>{item.name}</button>
+            ) : (
+              <button onClick={() => select(index)} className={styles.selButton}>{item.name}</button>
+            )
+          ))
+        )}
       </div>
       <div className={styles.info}>
         <div style={{ margin: "auto" }}>
-          12/1 ~ 12/3 여행
+          {dayjs(date[0]?.start).format('YY-MM-DD')}
           <br />
           맛집투어
         </div>
       </div>
       <div className={styles.pickdate}>
-        <button className={styles.selButton2}>1일차</button>
-        <button className={styles.selButton2}>2일차</button>
-        <button className={styles.selButton2}>3일차</button>
+        {Array.from({ length: date[0]?.date }, (_, index) => (
+          index === selected2 ? (
+            <button onClick={() => select2(index)} className={styles.selButton2s}>{index}일차</button>
+          ) : (
+            <button onClick={() => select2(index)} className={styles.selButton2}>{index}일차</button>
+          )
+        ))}
       </div>
       <div className={styles.schedule}>
-        <div className={styles.ele}>
-          아산 피나클(명소)
-          <br />
-          15:00 ~ 18:00
-          <div style={{ textAlign: "right" }}>x</div>
-        </div>
+        
+          {data.length === 0 ? (
+            <p>아직 일정이 없습니다. 추가해주세요.</p>
+          ) : (
+            sch.map((item, index) => (
+              <div className={styles.ele}>
+                <div>{item.sight_id}</div>
+                <br/>
+                <div>{item.start} ~ {item.end}</div>
+                <div style={{ textAlign: "right" }}>x</div>
+                </div>
+            ))
+          )}
+          
+        
       </div>
     </div>
   );
